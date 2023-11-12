1a37452f9918cab0e9556071c3c55ba9c70f6534
Saeed Eldeeb <saeedeldeeb1@gmail.com>
2023-11-12 23:03:25 +0200

    Added login page

--- WheelWander/Program.cs
+++ WheelWander/Program.cs
@@ -1,14 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using WheelWander.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("WheelWanderDbContextConnection") ??
+                       throw new InvalidOperationException(
+                           "Connection string 'WheelWanderDbContextConnection' not found.");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<WheelWanderDbContext>(options => {
+builder.Services.AddDbContext<WheelWanderDbContext>(options =>
+{
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:WheelWanderDbContextConnection"]);
 });
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<WheelWanderDbContext>();
+builder.Services.AddSession(); 
+builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
@@ -20,10 +31,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Dashboard}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
