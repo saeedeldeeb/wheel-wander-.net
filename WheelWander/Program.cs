c61e093439d27bded4daa6d74df5d7405bd1df54
eldeeb <saeedeldeeb1@gmail.com>
2024-01-28 00:40:09 +0200

    Init feature

--- WheelWander/Program.cs
+++ WheelWander/Program.cs
@@ -1,7 +1,12 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using WheelWander.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using WheelWander.Helpers;
 using WheelWander.Repositories;
+using WheelWander.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("WheelWanderDbContextConnection") ??
@@ -13,17 +18,37 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<WheelWanderDbContext>(options =>
 {
     options.UseSqlServer(
-        builder.Configuration["ConnectionStrings:WheelWanderDbContextConnection"],
+        connectionString,
         x => x.UseNetTopologySuite());
 });
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<WheelWanderDbContext>();
+builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+        };
+    });
 builder.Services.AddSession(); 
 builder.Services.AddRazorPages();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "WheelWander", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -31,6 +56,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WheelWander v1"));
 }
 
 app.UseHttpsRedirection();
